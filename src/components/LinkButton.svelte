6cb294c8339a9164c663f4e603daa69d05c692d7
GautierPicon <bt2ay5d9@anonaddy.com>
2026-03-24 19:19:46 +0100

    fix: corrects the alignment of icons within the cell

--- src/components/LinkButton.svelte
+++ src/components/LinkButton.svelte
@@ -7,10 +7,10 @@
 
 <a
   {href}
-  class="flex items-center bg-[#ebebeb] rounded-[40px] md:rounded-[100px] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.25)] md:shadow-[0px_3px_2px_0px_rgba(0,0,0,0.25)] h-[50px] sm:h-[59px] md:h-[69px] px-[6px] gap-3 md:gap-4 hover:brightness-95 transition-all"
+  class="flex items-center bg-[#ebebeb] rounded-[40px] md:rounded-[100px] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.25)] md:shadow-[0px_3px_2px_0px_rgba(0,0,0,0.25)] h-[50px] sm:h-[59px] md:h-[69px] px-[6px] gap-3 md:gap-4 md:pl-3 hover:brightness-95 transition-all"
 >
   <div
-    class="shrink-0 w-[40px] h-[40px] sm:w-[47px] sm:h-[47px] md:w-[53px] md:h-[53px] overflow-hidden"
+    class=" rounded-xs shrink-0 w-[36px] h-[36px] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] overflow-hidden"
   >
     <img src={icon} alt="" class="w-full h-full object-cover" />
   </div>
